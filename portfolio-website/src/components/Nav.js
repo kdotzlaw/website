0df29affbb44be638341dac934b0c8ea8228a880
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2024-11-13 15:19:10 -0600

    feat: Add LinkedIn and GitHub icons to navigation

--- portfolio-website/src/components/Nav.js
+++ portfolio-website/src/components/Nav.js
@@ -5,14 +5,22 @@ const Nav = () => {
   return (
    <nav id='nav' className='bg-white p-4 fixed w-full z-50 top-0 shadow-lg ' >
     <div className="container mx-auto flex items-center justify-between">
-        <div id='nav-header' className=" font-bold text-2xl">
-            Katrina's Portfolio
-        </div>
-        <ul className="flex space-x-6">
+       
+        <ul className="top-0 right-0 flex space-x-4">
             <li id='nav-item' ><a href='#home' >Home</a></li>
             <li id='nav-item' ><a href='#about' >About</a></li>
             <li id='nav-item' ><a href='#projects' >Projects</a></li>
             <li id='nav-item' ><a href='#contact'>Contact</a></li>
+        </ul>
+        <ul className="">
+            
+            <li id='nav-item' >
+                <i class="fa-brands fa-linkedin" href='https://www.linkedin.com/in/kdotzlaw' > </i>
+            </li>
+            <li id='nav-item' >
+                <i class="fa-brands fa-github" href='https://github.com/kdotzlaw' > </i>
+                
+            </li>
         </ul>
     </div>
    </nav>
