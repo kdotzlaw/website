a03e95a7b57d9b67979f458d385c455da8384342
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2024-11-15 19:45:48 -0600

    publications toggle framework

--- portfolio-website/src/components/Publications.js
+++ portfolio-website/src/components/Publications.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Publications = () => {
+//toggles
+    const [p1Expanded, setP1Expanded] = useState(false);
+    const [p2Expanded, setP2Expanded] = useState(false);
+    const [p3Expanded, setP3Expanded] = useState(false);
+    const toggleP1 = () => {
+        setP1Expanded(!p1Expanded);
+    };
+    const toggleP2 = () => {
+        setP2Expanded(!p2Expanded);
+    };
+    const toggleP3 = () => {
+        setP3Expanded(!p3Expanded);
+    };
+
     return (
-        <div id='publications-body' className="pr-64"> {/* Added padding-right to account for sidebar */}
+        <div id='body' className="pr-64"> {/* Added padding-right to account for sidebar */}
             <div id='publications-header' className="py-16">
-                <div className="container mx-auto">
-                    <h1 className="text-6xl font-bold text-center">Publications</h1>
+                <div className="container mx-auto text-left text-pretty max-w-3xl" >
+                    <h1 className="text-6xl font-bold ">Publications</h1>
                     <h2 className='py-4'>Subheading</h2>
                 </div>
             </div>
-            <div id='publications-content' className="container mx-auto py-4">
-                <div id='bio-container'>
-                    <h2 className='text-xl'>About Me</h2>
-                    <p className='py-4'>Bio goes here</p>
-                </div>
-                <div id='curr-proj-container'>
-                    <h2 className='text-xl'>Current Projects</h2>
-                    <p className='py-4'>Projects go here</p>
-                </div>
+            <div id='publications-content' className="container mx-auto text-left max-w-3xl">
+                <h2 className='py-4 font-bold cursor-pointer flex items-center'onClick={toggleP1}>
+                    Title
+                    <span className={`ml-2 transform ${p1Expanded ? 'rotate-180' : ''} transition-transform duration-200`}>▼</span>
+                </h2>
+                {p1Expanded && (
+                    <article className='text-pretty'>
+                        <p className='py-2 text-wrap'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                        </article>
+                )}
             </div>
         </div>
     );
