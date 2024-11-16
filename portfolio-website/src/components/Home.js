605095a5922f49135b1f3397573be492f3a48746
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2024-11-15 19:01:58 -0600

    feat: Add Katrina Dotzlaw's portfolio website content

--- portfolio-website/src/components/Home.js
+++ portfolio-website/src/components/Home.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import '../styles/Home.css';
 
+
 const Home = () => {
     
   return (
     <div id='home-body' className="pr-64"> {/* Added padding-right to account for sidebar */}
       <div id='home-header' className="py-16" >
-        <div className="container mx-auto text-left" >
-            <h1 className="text-8xl font-bold ">Hello World</h1>
-            <h2 className=' text-4xl py-8'>Job Title</h2>
+        <div className="container mx-auto text-left text-pretty" >
+            <h1 className="text-8xl font-bold ">Katrina Dotzlaw</h1>
+            <h2 className=' text-4xl py-8'>Software Developer</h2>
         
             <h2 className='text-2xl'>About Me</h2>
-            <p className='py-4'>Bio goes here</p>
-        
+            <img src="" alt=""/>
+            <article  className='py-4 text-pretty'>
+            <p>
+                I am a recent graduate of the University of Manitoba with a Bachelor's degree in Computer Science,
+                specializing in Software Engineering, Artificial Intelligence, and Databases.
+
+
+                
+                
+            </p>
+            </article>
+            
         </div>
       </div>
-      <div id='home-content' className="container mx-auto py-4 text-center">
+      <div id='home-content' className="container mx-auto py-4 text-right">
         <div id='curr-proj-container mx-auto text-right'>
             <h2 className='text-2xl'>Current Projects</h2>
             <p className='py-4'>Projects go here</p>
--- portfolio-website/src/components/Projects.js
+++ portfolio-website/src/components/Projects.js
@@ -1,24 +1,59 @@
 
 import React from 'react';
+import '../styles/Home.css';
 const Projects = () => {
     return (
-        <div id='projects-body' className="pr-64"> {/* Added padding-right to account for sidebar */}
+        <div id='body' className="pr-64"> {/* Added padding-right to account for sidebar */}
             <div id='projects-header' className="py-16">
-                <div className="container mx-auto">
-                    <h1 className="text-6xl font-bold text-center">Projects</h1>
-                    <h2 className='py-4'>Subheading</h2>
+                <div className="container mx-auto text-left">
+                    <h1 className="text-6xl font-bold ">Projects</h1>
+                    <div id='frontend-dev' className='py-4'>
+                        <h2 className='py-4 font-bold'>Frontend Development</h2>
+                        <article>
+                            <a href="https://www.peacelc.com" id='text-link' >
+                                <h3>Peace Lutheran Church Website</h3>
+                            </a>
+                            <p className='py-2'>
+                                Built using Wordpress and custom HTML/CSS. 
+
+                            </p>
+                        </article>
+                    
+                    </div>
+                    <div id='backend-dev' className='py-4'>
+                    <h2 className='py-4 font-bold'>Backend Development</h2>
+                    <article>
+                            <a href="" id='text-link' >
+                                <h3>head</h3>
+                            </a>
+                            <p className='py-2'>
+                                text
+
+                            </p>
+                        </article>
+                    </div>
+                    <div id='data-science' className='py-4 '>
+                    <h2 className='py-4 font-bold'>Machine Learning & Data Science</h2>
+                    <article className='text-wrap'>
+                            <a href="https://github.com/kdotzlaw/UniversityProjects/blob/main/ESN-MachineLearning/ESN.ipynb" id='text-link' >
+                                <h3>Echo State Network</h3>
+                            </a>
+                            <p id='project-content'className='py-2'>
+                            A type of recurrent neural network created with Python to recognize temporal patterns like k-step ahead forecasting. 
+                            Specifically, ESNs use backpropagation to feed optimized hyperparameters back into its recurrent internal state.
+                            </p>
+                            <img src=''alt='An example of an ESN'></img>
+                            <p id='project-content'className='py-2'>
+                                Explores machine learning concepts like error calculation via MSE, backpropagation, ridge regression,
+                                 sigmoid activation functions, and hyperparameter tuning.
+                                All visualization was done using matplotlib and numpy. 
+                            </p>
+                        </article>
+                    </div>
                 </div>
+                
             </div>
-            <div id='projects-content' className="container mx-auto py-4">
-                <div id='bio-container'>
-                    <h2 className='text-xl'>About Me</h2>
-                    <p className='py-4'>Bio goes here</p>
-                </div>
-                <div id='curr-proj-container'>
-                    <h2 className='text-xl'>Current Projects</h2>
-                    <p className='py-4'>Projects go here</p>
-                </div>
-            </div>
+            
         </div>
     );
 }
