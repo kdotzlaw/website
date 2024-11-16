3137c889ecfc75e026ae79b9ef429931cd4d97f9
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2024-11-15 19:33:55 -0600

    project tabs are collapsable

--- portfolio-website/src/components/Projects.js
+++ portfolio-website/src/components/Projects.js
@@ -4,10 +4,18 @@ import '../styles/Home.css';
 import esnImage from '../img/esn.JPG';
 
 const Projects = () => {
+    //Toggle each project section
     const [frontendExpanded, setFrontendExpanded] = useState(false);
-
+    const [backendExpanded, setBackendExpanded] = useState(false);
+    const [dataExpanded, setDataExpanded] = useState(false);
     const toggleFrontend = () => {
         setFrontendExpanded(!frontendExpanded);
+    };
+    const toggleBackend = () => {
+        setBackendExpanded(!backendExpanded);
+    };
+    const toggleData = () => {
+        setDataExpanded(!dataExpanded);
     };
     return (
         <div id='body' className="pr-64"> {/* Added padding-right to account for sidebar */}
@@ -15,7 +23,7 @@ const Projects = () => {
                 <div className="container mx-auto text-left max-w-3xl">
                     <h1 className="text-6xl font-bold ">Projects</h1>
                     <div id='frontend-dev' className='py-4'>
-                        <h2 className='py-4 font-bold cursor-pointer flex items-center' onClick={toggleFrontend}>
+                        <h2 id='header-indv-project' className='py-4 font-bold cursor-pointer flex items-center' onClick={toggleFrontend}>
                             Frontend Development
                             <span className={`ml-2 transform ${frontendExpanded ? 'rotate-180' : ''} transition-transform duration-200`}>▼</span>
                         </h2>
@@ -31,7 +39,10 @@ const Projects = () => {
                         )}
                     </div>
                     <div id='backend-dev' className='py-4'>
-                    <h2 className='py-4 font-bold'>Backend Development</h2>
+                    <h2 id='header-indv-project' className='py-4 font-bold cursor-pointer flex items-center' onClick={toggleBackend}>Backend Development
+                    <span className={`ml-2 transform ${backendExpanded ? 'rotate-180' : ''} transition-transform duration-200`}>▼</span>
+                    </h2>
+                    {backendExpanded && (
                     <article>
                             <a href="" id='text-link' >
                                 <h3>head</h3>
@@ -40,25 +51,31 @@ const Projects = () => {
                                 text
                             </p>
                         </article>
+                    )}
                     </div>
                     <div id='data-science' className='py-4 '>
-                    <h2 className='py-4 font-bold'>Machine Learning & Data Science</h2>
-                    <article className='text-wrap'>
-                            <a href="https://github.com/kdotzlaw/UniversityProjects/blob/main/ESN-MachineLearning/ESN.ipynb" id='text-link' >
-                                <h3>Echo State Network</h3>
-                            </a>
-                            <p id='project-content' className='py-2 text-wrap'>
-                            A type of recurrent neural network created with Python to recognize temporal patterns like k-step ahead forecasting. 
-                            Specifically, ESNs use backpropagation to feed optimized hyperparameters back into its recurrent internal state.
-                            </p>
-                            <p id='project-content' className='py-2 text-wrap'>
-                                Explores machine learning concepts like error calculation via MSE, backpropagation, ridge regression,
-                                 sigmoid activation functions, and hyperparameter tuning.
-                                All visualization was done using matplotlib and numpy. 
-                            </p>
-                            <img src={esnImage} alt='An example of an ESN' className="w-[900px] h-[600px] object-cover rounded my-4 cursor-pointer hover:scale-105 transition-transform duration-300" onClick={() => window.open(esnImage, '_blank')}/>
-                            
-                        </article>
+                    <h2 id='header-indv-project'className='py-4 font-bold cursor-pointer flex items-center' onClick={toggleData}>
+                        Machine Learning & Data Science
+                        <span className={`ml-2 transform ${dataExpanded ? 'rotate-180' : ''} transition-transform duration-200`}>▼</span>
+                    </h2>
+                    {dataExpanded && (
+                        <article className='text-wrap'>
+                                <a href="https://github.com/kdotzlaw/UniversityProjects/blob/main/ESN-MachineLearning/ESN.ipynb" id='text-link' >
+                                    <h3>Echo State Network</h3>
+                                </a>
+                                <p id='project-content' className='py-2 text-wrap'>
+                                A type of recurrent neural network created with Python to recognize temporal patterns like k-step ahead forecasting. 
+                                Specifically, ESNs use backpropagation to feed optimized hyperparameters back into its recurrent internal state.
+                                </p>
+                                <p id='project-content' className='py-2 text-wrap'>
+                                    Explores machine learning concepts like error calculation via MSE, backpropagation, ridge regression,
+                                    sigmoid activation functions, and hyperparameter tuning.
+                                    All visualization was done using matplotlib and numpy. 
+                                </p>
+                                <img src={esnImage} alt='An example of an ESN' className="w-[900px] h-[600px] object-cover rounded my-4 cursor-pointer hover:scale-105 transition-transform duration-300" onClick={() => window.open(esnImage, '_blank')}/>
+                                
+                            </article>
+                        )}
                     </div>
                 </div>
                 
