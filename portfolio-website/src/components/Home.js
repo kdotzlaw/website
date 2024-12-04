d0dc0713028a30064626e3a8e4dc5f27dab60253
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2024-12-04 14:13:23 -0600

    feat: Add padding-top to home section and center header text

--- portfolio-website/src/components/Home.js
+++ portfolio-website/src/components/Home.js
@@ -4,15 +4,15 @@ import profilePic from '../img/kat1.jpg';
 
 const Home = () => {
   return (
-    <div id='body' className='pt-12 bg-gunmetal text-white'> {/* Added padding-top to account for header navigation */}
+    <div id='body' className='pt-12 bg-gunmetal text-white '> {/* Added padding-top to account for header navigation */}
       <div id='home-header' className="py-16">
-        <div className="container mx-auto text-left text-pretty max-w-3xl px-4">
+        <div className="container mx-auto text-center text-pretty max-w-3xl px-4">
           <h1 className="py-4 text-6xl md:text-8xl font-bold">Katrina Dotzlaw</h1>
           <h2 className='text-3xl md:text-4xl py-8'>Software Developer</h2>
           <div className="py-4 text-pretty flex-1 bg-teal bg-opacity-10 rounded-lg p-6">
             <div className="flex flex-col md:flex-row md:items-start space-y-6 md:space-y-0 md:space-x-8 text-left bg-gunmetal shadow-lg rounded-lg overflow-hidden card">
               <img src={profilePic} alt="Katrina Dotzlaw" className="w-64 h-64 object-cover rounded-full"/>
-              <div className="flex-1 card">
+              <div className="flex-1 card" id='about'>
                 <h2 className='text-3xl mb-4'>About Me</h2>
                 <article className='text-pretty'>
                   <p className="text-wrap">
@@ -22,7 +22,7 @@ const Home = () => {
                 </article>
               </div>
             </div>
-            <div className='text-left bg-gunmetal shadow-lg rounded-lg overflow-hidden card mt-8'>
+            <div className='text-left bg-gunmetal shadow-lg rounded-lg overflow-hidden card mt-8' id='tech'>
               <article className='py-4 text-pretty flex-1'>
                 <h2 className="text-3xl mb-4 px-4">Skills & Technologies</h2>
                 <p className="text-wrap px-4">
