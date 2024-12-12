2cec1c7a3762ce0a6298ffa9205ae65fc1e0d168
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2024-12-12 12:03:22 -0600

    Project-Tile component and Carousel display are responsive

--- portfolio-website/src/components/Project-Tiles.js
+++ portfolio-website/src/components/Project-Tiles.js
@@ -41,124 +41,68 @@ const Project_Tiles = () => {
 
 
 
-     return(
-        <div id = 'projects' className='pt-12 px-12  text-white'>
-            <div className='container mx-auto px-12 py-16'>
-                <div className="max-w-6xl mx-auto flex flex-col items-center ">
-                    <h1 className='text-6xl font-bold mb-12 text-center' id='project-tile-header'>Skills & Technologies</h1>
-                    {/*Project Category Tiles   <div className="h-[1280px] w-[1280px] mx-auto ">*/}
-                    <div className={`h-[1280px] w-[1280px] mx-auto flex flex-col items-center justify-start transition-all duration-300 ${
-                    selectedCategory ? 'h-[2000px]' : 'h-[1280px]'
-                }`}>
-                        <div className="max-w-5xl mx-auto  rounded-lg p-6">
-                            <div id='project-tiles' className='grid grid-cols-2 lg:grid-cols-4 gap-6 p-6 project-tile'>
-                                {categories.map((category)=>(
-                                    <div key={category.id} onClick={()=>{
-                                        setSelectedCategory(prevCategory => prevCategory === category.id ? null : category.id);
-                                        setCurrIndex(0);
-                                        /*setSelectedCategory(category.id);
-                                        setCurrIndex(0);*/
-                                    }}
-                                
-                                    className={` p-6 rounded-lg cursor-pointer flex flex-col items-center justify-center 
-                                        transition-all duration-300 transform hover:scale-105 
-                                        ${selectedCategory === category.id ? 'bg-teal bg-opacity-20' : 'bg-white bg-opacity-10'  } `
+    return(
+        <div id='projects' className='min-h-screen pt-16 sm:pt-20 px-4 sm:px-8 lg:px-12 text-white'>
+            <div className='container mx-auto px-4 sm:px-8 lg:px-12'>
+                <div className="max-w-6xl mx-auto flex flex-col items-center">
+                    <h1 className='text-4xl sm:text-5xl lg:text-6xl font-bold mb-8 sm:mb-12 text-center mt-8 sm:mt-16'>Skills & Technologies</h1>
+                    <div className={`w-full transition-all duration-300 ${
+                        selectedCategory ? 'min-h-[2000px]' : 'min-h-[800px] sm:min-h-[1000px] lg:min-h-[1280px]'
+                    }`}>
+                        <div className="max-w-5xl mx-auto rounded-lg p-4 sm:p-6">
+                            <div id='project-tiles' className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 sm:gap-4 lg:gap-6 p-4 sm:p-6 project-tile'>
+                                {categories.map((category) => (
+                                    <div 
+                                        key={category.id} 
+                                        onClick={() => {
+                                            setSelectedCategory(prevCategory => 
+                                                prevCategory === category.id ? null : category.id
+                                            );
+                                            setCurrIndex(0);
+                                        }}
+                                        className={`p-3 sm:p-4 lg:p-6 rounded-lg cursor-pointer flex flex-col items-center justify-center 
+                                            transition-all duration-300 transform hover:scale-105 
+                                            ${selectedCategory === category.id ? 'bg-teal bg-opacity-20' : 'bg-white bg-opacity-10'}`
                                         }
                                     >
                                         <img 
-                                            src = {category.icons}
-                                            alt = {category.name}
-                                            className="w-12 h-12 mx-auto mb-2 object-contain"
+                                            src={category.icons}
+                                            alt={category.name}
+                                            className="w-8 h-8 sm:w-10 sm:h-10 lg:w-12 lg:h-12 mx-auto mb-2 object-contain"
                                         />
-                                    <h3 className="text-sm font-semibold text-center">{category.name}</h3>
-                                </div>
+                                        <h3 className="text-xs sm:text-sm lg:text-base font-semibold text-center">
+                                            {category.name}
+                                        </h3>
+                                    </div>
                                 ))}
-                            </div> {/*End Project Tiles*/}
-                        </div>
-                    
-                    
-                    {/*Project Carousel
-                    {selectedCategory && (
-                        <div className='mt-12 relative'>
-                            <div className='flex items-center justify-center gap-4'>
-                                <button onClick={prevSlide} className='p-2 rounded-full bg-teal bg-opacity-20 hover:bg-opacity-30 carousel-button'><ChevronLeft className="w-6 h-6" /></button>
-                                <div className="w-full max-w-4xl bg-white bg-opacity-5 rounded-lg p-6">
-                                    {getProjectsInCategory(selectedCategory)[currIndex] && (
-                                            <div className='flex flex-col md:flex-row gap-8'>
-                                                <div className='md:w-1/3'>
-                                                    {getProjectsInCategory(selectedCategory)[currIndex].images.map((image, index) => (
-                                                        <img 
-                                                            key={index}
-                                                            src={image.src} 
-                                                            alt={image.alt} 
-                                                            className="w-full h-auto object-contain rounded mb-4"
-                                                        />
-                                                    ))}
-                                                    {getProjectsInCategory(selectedCategory)[currIndex].url &&(
-                                                        <button onClick={() => window.open(getProjectsInCategory(selectedCategory)[currIndex].url)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">View Project</button>
-                                                    )}
-                                                </div>
-                                                <div className='md:w-2/3'>
-                                                    <h2 className='text-2xl font-bold mb-4'>{getProjectsInCategory(selectedCategory)[currIndex].header}</h2>
-                                                    <p className='mb-4'>{getProjectsInCategory(selectedCategory)[currIndex].description}</p>
-                                                    {getProjectsInCategory(selectedCategory)[currIndex].sub && (
-                                                        <>
-                                                            <h3 className="text-xl font-semibold mb-2">{getProjectsInCategory(selectedCategory)[currIndex].sub}</h3>
-                                                            <p className="mb-4">{getProjectsInCategory(selectedCategory)[currIndex].sub_content}</p>
-                                                        </>
-                                                    )}
-                                                    {getProjectsInCategory(selectedCategory)[currIndex].footer && (
-                                                        <div className='md-4'>
-                                                            <h3 className='text-xl font-semibold mb-2'>{getProjectsInCategory(selectedCategory)[currIndex].footer}</h3>
-                                                            <ul className='list-disc list-inside'>
-                                                                {getProjectsInCategory(selectedCategory[currIndex].result?.map((item, index)=>(
-                                                                    item.type === 'text' ? <li key={index} className='text-wrap'>{item.content}</li> : item.type === 'image-group' ? (
-                                                                        <div key={index} className='flex flex-row space-x-4'>
-                                                                            {item.images.map((img, imgIndex) => (
-                                                                                <div key={imgIndex} className='flex flex-col items-center w-1/2'>
-                                                                                    <img src={img.src} alt={img.alt} className='w-full h-auto object-contain rounded hover:scale-105 transition-transform duration-300' />
-                                                                                    {img.descriptor && <p className='mt-2 text-sm text-gray-600'>{img.descriptor}</p>}
-                                                                                </div>
-                                                                            ))}
-                                                                        </div>
-                                                                    ) : (
-                                                                        <div key={index} className='flex flex-col items-center'>
-                                                                            <img src={item.src} alt={item.alt} className='w-full h-auto object-contain rounded hover:scale-105 transition-transform duration-300' />
-                                                                            {item.descriptor && <p className='mt-2 text-sm text-gray-600'>{item.descriptor}</p>}
-                                                                        </div>
-                                                                    )
-                                                                )))}
-                                                            </ul>
-                                                        </div>
-                                                    )}
-                                                </div>
-                                            </div>
-                                    ) }
-                                </div> {/*End Carousel Content
-                                <button onClick={nextSlide} className='p-2 rounded-full bg-teal bg-opacity-20 hover:bg-opacity-30 carousel-button'><ChevronRight className="w-6 h-6" /></button>
-                            </div> 
-                        </div>
-                    )}*/}
-            
-                <div className='flex items-center justify-center gap-4 mt-8  w-full'> 
-                    {selectedCategory && (
-                         <div className='flex items-center justify-center gap-4 mt-8 w-full max-w-5xl '> 
-                            <button onClick={prevSlide} className='p-2 rounded-full bg-teal bg-opacity-20 hover:bg-opacity-30 carousel-button carousel-nav'>
-                                <ChevronLeft className="w-6 h-6" />
-                            </button>
-                            <div className='flex-1 max-w-4xl'>
-                                <ProjectCarousel project={getProjectsInCategory(selectedCategory)[currIndex]} />
                             </div>
-                            <button onClick={nextSlide} className='p-2 rounded-full bg-teal bg-opacity-20 hover:bg-opacity-30 carousel-button carousel-nav'>
-                                <ChevronRight className="w-6 h-6" />
-                            </button>
-                     </div>
-                )}
+                        </div>
+                    
+                        <div className='flex items-center justify-center gap-4 mt-8 w-full'> 
+                            {selectedCategory && (
+                                <div className='flex items-center justify-center gap-4 mt-8 w-full max-w-5xl'> 
+                                    <button 
+                                        onClick={prevSlide} 
+                                        className='p-2 rounded-full bg-teal bg-opacity-20 hover:bg-opacity-30 carousel-nav hidden sm:block'
+                                    >
+                                        <ChevronLeft className="w-6 h-6" />
+                                    </button>
+                                    <div className='flex-1 max-w-4xl'>
+                                        <ProjectCarousel project={getProjectsInCategory(selectedCategory)[currIndex]} />
+                                    </div>
+                                    <button 
+                                        onClick={nextSlide} 
+                                        className='p-2 rounded-full bg-teal bg-opacity-20 hover:bg-opacity-30 carousel-nav hidden sm:block'
+                                    >
+                                        <ChevronRight className="w-6 h-6" />
+                                    </button>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-     );  
+    );
 };
     export default Project_Tiles;
