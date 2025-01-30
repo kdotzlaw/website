77669bc1642bac5b054a5d2ebbb93044c0a45507
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2025-01-30 11:51:39 -0600

    refactor: Remove email input and update contact form submit button style

--- portfolio-website/src/components/Contact.js
+++ portfolio-website/src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import '../styles/main.css';
 const Contact = ({ onClose }) => {
     const [formData, setFormData] = useState({
         name: '',
@@ -65,18 +65,7 @@ const Contact = ({ onClose }) => {
                             required
                         />
                     </div>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-white-700 text-sm font-bold mb-2">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 bg-white-700 leading-tight focus:outline-none focus:shadow-outline"
-                            required
-                        />
-                    </div>
+                  
                     <div className="mb-6">
                         <label htmlFor="message" className="block text-white-700 text-sm font-bold mb-2">Message</label>
                         <textarea
@@ -89,7 +78,7 @@ const Contact = ({ onClose }) => {
                         ></textarea>
                     </div>
                     <div className="flex items-center justify-between">
-                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        <button type="submit" className="btn-contact-submit text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                             Send Message
                         </button>
                     </div>
